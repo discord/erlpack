f77835b0afd79cf1ec4c223b571a9108b1087c86
mrkcsc <miguel@miguelgaeta.com>
2016-04-22 17:49:25 -0700

    Added unit test.

---
+++ java/erlpack/src/main/java/com/discordapp/erlpack/Erlpack.java
@@ -0,0 +1,4 @@
+package com.discordapp.erlpack;
+
+public class Erlpack {
+}
---
+++ java/erlpack/src/test/java/com/discordapp/erlpack/ErlpackUnitTest.java
@@ -0,0 +1,15 @@
+package com.discordapp.erlpack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ErlpackUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
