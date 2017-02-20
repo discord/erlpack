da609b741941f830ced7403dbaac7e38aa2ec596
Stanislav Vishnevskiy <vishnevskiy@gmail.com>
2017-02-20 14:37:50 -0800

    Add ErlPack for Elixir

---
+++ ex/erlpack/test/erlpack_test.exs
@@ -0,0 +1,5 @@
+defmodule ErlPackTest do
+  use ExUnit.Case
+  
+  doctest ErlPack
+end
---
+++ mix.exs
@@ -0,0 +1,40 @@
+defmodule ErlPack.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :erlpack,
+      version: "1.0.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      package: package(),
+      elixirc_paths: ["ex/erlpack/lib/"],
+      test_paths: ["ex/erlpack/test/"]
+    ]
+  end
+
+  def application do
+    [
+      applications: []
+    ]
+  end
+
+  defp deps do
+    []
+  end
+
+  def package do
+    [
+      name: :erlpack,
+      description: "High Performance Erlang Term Format Packer",
+      maintainers: [],
+      licenses: ["MIT"],
+      files: ["ex/erlpack/lib/*", "mix.exs", "README*", "LICENSE*"],
+      links: %{
+        "GitHub" => "https://github.com/hammerandchisel/fastglobal",
+      },
+    ]
+  end
+end
